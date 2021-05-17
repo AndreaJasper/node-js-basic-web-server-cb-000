1368ebd73d85c4cf436c59a0656d3cd395ea8a3c
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-17 14:27:13 +0000

    Automatically backed up by Learn

--- server.js
+++ server.js
@@ -1,66 +1,73 @@
 "use strict";
 
+
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
 const bodyParser   = require('body-parser');
 const urlParser    = require('url');
 const querystring  = require('querystring');
-const bycrypt      = require('bycrypt');
+const bcrypt       = require('bcrypt');
 
-const router = new Router({ mergerParams: true});;
+const router = new Router({ mergerParams: true });
 let messages = [];
-let nextId = [];
+let nextId = 1;
 
-//bodyparser handles post data
+class Message {
+  constructor(message) {
+    this.id = nextId;
+    this.message = message;
+    nextId++;
+  }
+}
+
+// Use body parser to handle post data.
 router.use(bodyParser.json());
 
-//response from the root path
+//Get a simple response from root path
 router.get('/', (request, response) => {
-  response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.end('Hello, World!');
 });
 
-//sets a message by the POST data
+//set a message by POST data
 router.post('/message', (request, response) => {
   let newMsg;
-  
-  response.setHeader('Content-Type', 'application/json'; charset=utf-8');
-  
-  if(!request.body.message) {
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  if (!request.body.message) {
     response.statusCode = 400;
     response.statusMessage = 'No message provided.';
     response.end();
     return;
   }
-  
+
   newMsg = new Message(request.body.message);
   messages.push(newMsg);
-  
+
   response.end(JSON.stringify(newMsg.id));
-  
 });
 
-
-//gets the list of messages from /messages
-
+//Get list of messages from /messages
 router.get('/messages', (request, response) => {
   let url = urlParser.parse(request.url),
-    params = querystring.parse(url.query);
-    
+      params = querystring.parse(url.query);
+
   let result = JSON.stringify(messages);
-  
+
   response.setHeader('Content-Type', 'application/json; charset=utf-8');
-  
+
   if (params.encrypt) {
     response.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    return bcrypt.has(result, 10, (error, hashed) => {
+    return bcrypt.hash(result, 10, (error, hashed) => {
       if (error) {
-        thorw new Error();
+        throw new Error();
       }
       response.end(hashed);
     });
   }
+
   response.end(result);
 });
 
